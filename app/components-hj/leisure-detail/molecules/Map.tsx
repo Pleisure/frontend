7e29654e52c260df41ba62eb82450bcd404a14de
hyun joo oh <58460131+hyunjoo1130@users.noreply.github.com>
2023-08-18 18:31:22 +0900

    PLS-58 Feat:Add Kakao map to leisure detail page

--- app/components-hj/leisure-detail/molecules/Map.tsx
+++ app/components-hj/leisure-detail/molecules/Map.tsx
@@ -1,3 +1,40 @@
+'use client';
+
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 export default function Map() {
-  return <div className="w-full h-64 mt-6 border-[1px] border-grayTxt"> </div>;
+  useEffect(() => {
+    const kakaoMapScript = document.createElement('script');
+    kakaoMapScript.async = true;
+    kakaoMapScript.src =
+      '//dapi.kakao.com/v2/maps/sdk.js?appkey=631346306c13cc99e35393e71852c215&autoload=false';
+    document.head.appendChild(kakaoMapScript);
+
+    const onLoadKakaoAPI = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById('kakao-map');
+        const options = {
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+          level: 3, //지도의 레벨(확대, 축소 정도)
+        };
+
+        const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+      });
+    };
+
+    kakaoMapScript.addEventListener('load', onLoadKakaoAPI);
+  }, []);
+
+  return (
+    <div
+      className="w-full h-64 mt-6 border-[1px] border-grayTxt"
+      id="kakao-map"
+    ></div>
+  );
 }
