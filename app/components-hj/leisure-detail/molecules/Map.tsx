675d601a642fceb6b4b23d441187acd6458e08fc
hyun joo oh <58460131+hyunjoo1130@users.noreply.github.com>
2023-08-21 14:53:38 +0900

    PLS-78 Feat:Add Marker on map (latitude, longitude)

--- app/components-hj/leisure-detail/molecules/Map.tsx
+++ app/components-hj/leisure-detail/molecules/Map.tsx
@@ -20,11 +20,16 @@ export default function Map() {
       window.kakao.maps.load(() => {
         const container = document.getElementById('kakao-map');
         const options = {
-          center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+          center: new window.kakao.maps.LatLng(37.752532, 127.535929), //지도의 중심좌표. ⭐️ 위도, 경도를 주소에 맞게 받아야 함
           level: 3, //지도의 레벨(확대, 축소 정도)
         };
 
         const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+        const marker = new window.kakao.maps.Marker({
+          position: map.getCenter(),
+        });
+        marker.setMap(map);
       });
     };
 
