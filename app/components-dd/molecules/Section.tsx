5ce69fbc76a396630922ac057c9335ea7885be35
Daeun Kim <daeunkim212@gmail.com>
2023-09-01 04:54:23 +0900

    PLS-106 Feat:Add isHeightFull option to section component

--- app/components-dd/molecules/Section.tsx
+++ app/components-dd/molecules/Section.tsx
@@ -1,10 +1,13 @@
+import joinClassNames from '@/app/libs-dd/utils/joinClassNames';
+
 interface Props {
+  isHeightFull?: boolean;
   children: React.ReactNode;
 }
 
-export default function Section({ children }: Props) {
+export default function Section({ isHeightFull = false, children }: Props) {
   return (
-    <section className='w-full bg-white shadow-md'>
+    <section className={joinClassNames('w-full bg-white shadow-md', isHeightFull ? 'h-full' : '')}>
       <div className='w-full'>{children}</div>
     </section>
   );
