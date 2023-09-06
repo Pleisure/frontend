d41fdfe7c164a64257fe4ee77b8dd7df92fa79e6
hyun joo oh <58460131+hyunjoo1130@users.noreply.github.com>
2023-09-06 22:16:55 +0900

    PLS-93 Modify:[FE] Implement review report modal

--- app/components-hj/leisure-detail/organisms/Review.tsx
+++ app/components-hj/leisure-detail/organisms/Review.tsx
@@ -12,37 +12,39 @@ export default function Review() {
   const [showModal, setShowModal] = useState<boolean>(false);
 
   return (
-    <section className="relative px-5 review">
-      <Rating />
-      <Dropdown />
-      {REVIEW_ARTICLE_DATA.map((data, i, currentArr) => {
-        return (
-          <ReviewArticle
-            key={data.storeReviewId}
-            dataIdx={i}
-            dataLength={currentArr.length}
-            reviewWriterProfilePictureUrl={data.reviewWriterProfilePictureUrl}
-            reviewWriterNickname={data.reviewWriterNickname}
-            createdAt={data.createdAt}
-            reviewWriterAge={data.reviewWriterAge}
-            reviewWriterGender={data.reviewWriterGender}
-            storeReviewId={data.storeReviewId}
-            storeReviewContent={data.storeReviewContent}
-            storeReviewRating={data.storeReviewRating}
-            storeReviewPhotoUrls={data.storeReviewPhotoUrls}
-            setShowModal={setShowModal}
-          />
-        );
-      })}
-      <Modal showModal={showModal} setShowModal={setShowModal} />
-      <Button
-        divStyle="all-review-btn"
-        buttonStyle="w-full bg-white btn btn-active border-1 border-cyanTxt text-cyanTxt hover:bg-white hover:border-cyanTxt"
-        content="모든 리뷰 보기"
-      />
+    <>
+      <section className="relative px-5 review">
+        <Rating />
+        <Dropdown />
+        {REVIEW_ARTICLE_DATA.map((data, i, currentArr) => {
+          return (
+            <ReviewArticle
+              key={data.storeReviewId}
+              dataIdx={i}
+              dataLength={currentArr.length}
+              reviewWriterProfilePictureUrl={data.reviewWriterProfilePictureUrl}
+              reviewWriterNickname={data.reviewWriterNickname}
+              createdAt={data.createdAt}
+              reviewWriterAge={data.reviewWriterAge}
+              reviewWriterGender={data.reviewWriterGender}
+              storeReviewId={data.storeReviewId}
+              storeReviewContent={data.storeReviewContent}
+              storeReviewRating={data.storeReviewRating}
+              storeReviewPhotoUrls={data.storeReviewPhotoUrls}
+              setShowModal={setShowModal}
+            />
+          );
+        })}
+        <Modal showModal={showModal} setShowModal={setShowModal} />
+        <Button
+          divStyle="all-review-btn"
+          buttonStyle="w-full bg-white btn btn-active border-1 border-cyanTxt text-cyanTxt hover:bg-white hover:border-cyanTxt"
+          content="모든 리뷰 보기"
+        />
+      </section>
       {showModal && (
-        <div className="fixed top-0 left-[28.35%] m-auto max-w-md w-full h-screen bg-[rgba(0,0,0,0.5)] z-[100]" />
+        <div className="fixed top-0 max-w-md w-full h-screen bg-[rgba(0,0,0,0.5)] z-[100]" />
       )}
-    </section>
+    </>
   );
 }
